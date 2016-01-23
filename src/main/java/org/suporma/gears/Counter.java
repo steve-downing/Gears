d3504f9fb222f03025473e7f2a13c81afafd57b4
Steve Downing <ssdowning@gmail.com>
2016-01-23 15:28:57 -0800

    hashCode and equals

--- src/main/java/org/suporma/gears/Counter.java
+++ src/main/java/org/suporma/gears/Counter.java
@@ -25,4 +25,29 @@ public class Counter<T> {
     public int getCount(T val) {
         return counts.getOrDefault(val, 0);
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((counts == null) ? 0 : counts.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Counter<?> other = (Counter<?>) obj;
+        if (counts == null) {
+            if (other.counts != null)
+                return false;
+        } else if (!counts.equals(other.counts))
+            return false;
+        return true;
+    }
 }
